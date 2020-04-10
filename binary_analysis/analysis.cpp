7086dc99cddbd14ee47ae0a85da45b4898bb6bde
Xiang Ren <jenny.ren@mail.utoronto.ca>
2020-04-10 19:22:02 -0400

    Added simple function to get the immediate dominator given a function and address, and find the if condition from the dominator.

--- binary_analysis/analysis.cpp
+++ binary_analysis/analysis.cpp
@@ -41,15 +41,26 @@ BPatch_addressSpace* startInstrumenting(accessType_t accessType, const char* nam
   return handle;
 }
 
-int binaryAnalysis(BPatch_addressSpace *app) {
-    BPatch_image* appImage = app->getImage();
+void printLineInfo(BPatch_basicBlock *block) {
+    fprintf(stdout, "start %lx end %lx\n", block->getStartAddress(), block->getEndAddress());
+    BPatch_Vector<BPatch_sourceBlock*> sourceBlocks;
+    block->getSourceBlocks(sourceBlocks);
+    for (int i = 0; i < sourceBlocks.size(); i++) {
+        BPatch_Vector<unsigned short> lines;
+        sourceBlocks[i]->getSourceLines(lines);
+        for (int j = 0; j < lines.size(); j++) {
+            fprintf(stdout, "source line: %u.\n", lines[j]);
+        }
+    }
+}
+
+BPatch_basicBlock *getImmediateDominator(BPatch_image* appImage,  const char *func_name, long unsigned int addr) {
 
     vector<BPatch_function*> functions;
-    const char *func_name = "scanblock";
     appImage->findFunction(func_name, functions);
     if (functions.size() == 0) {
         fprintf(stderr, "Loading function %s failed.\n", func_name);
-        return -1;
+        return NULL;
     } else if (functions.size() > 1) {
         fprintf(stderr, "More than one function with name %s, using one.\n", func_name);
     }
@@ -59,23 +70,31 @@ int binaryAnalysis(BPatch_addressSpace *app) {
     set<BPatch_basicBlock*> blocks;
     fg->getAllBasicBlocks(blocks);
 
+    BPatch_basicBlock *target = NULL;
     for (auto block_iter = blocks.begin();
          block_iter != blocks.end();
          ++block_iter) {
-
         BPatch_basicBlock *block = *block_iter;
-        std::vector<InstructionAPI::Instruction> insns;
-        block->getInstructions(insns);
-        for (auto insn_iter = insns.begin();
-             insn_iter != insns.end();
-             ++insn_iter) {
-            InstructionAPI::Instruction insn = *insn_iter;
-            if (insn.readsMemory() || insn.writesMemory()) {
-                fprintf(stdout, "Instruction reads or writes memory\n");
-            }
+        //printLineInfo(block);
+        if (addr >= block->getStartAddress() && addr <= block->getEndAddress()) {
+            target = block;
+            break;
         }
     }
-    return 0;
+
+    if (target == NULL) {
+        fprintf(stderr, "Failed to find basic block for function %s @ %lx.\n", func_name, addr);
+        return NULL;
+    }
+    return target->getImmediateDominator();
+}
+
+InstructionAPI::Instruction findIfCondition(BPatch_basicBlock *block) {
+  std::vector<InstructionAPI::Instruction> insns;
+  block->getInstructions(insns);
+  InstructionAPI::Instruction ret = *insns.rbegin();
+  assert(ret.getCategory() == InstructionAPI::InsnCategory::c_BranchInsn);
+  return ret;
 }
 
 int main() {
@@ -85,12 +104,25 @@ int main() {
   const char* progArgv[] = {"test.zip"};
   accessType_t mode = open;
 
-  // Create/attach/open a binary
+  // Just open the binary for analysis, doesn't actually instrument it!
   BPatch_addressSpace* app = startInstrumenting(mode, progName, progPID, progArgv);
   if (!app) {
-  	fprintf(stderr, "startInstrumenting failed\n");
+  	fprintf(stderr, "opening the binary failed\n");
 	exit(1);
   }
 
-  binaryAnalysis(app);
+  BPatch_image* appImage = app->getImage();
+
+  // TODO, find the right address for the starting line ....
+  BPatch_basicBlock * immed_dom = getImmediateDominator(appImage, "scanblock", 0x4094ef);
+
+  InstructionAPI::Instruction ifCond = findIfCondition(immed_dom);
+
+  // find operands of an instruction, decide how to handle!
+  // => find the operator type
+  // => trace dataflow, find definition point?
+
+  // how to handle when there are multiple variables used in an instruction?
+
+
 }
