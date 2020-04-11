552af51cca03c8d0f50db86b75603e6fe8b9e0d3
Xiang Ren <jenny.ren@mail.utoronto.ca>
2020-04-11 01:08:14 -0400

    Playing with the slicing function of Dyninst.

--- binary_analysis/analysis.cpp
+++ binary_analysis/analysis.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <vector>
 #include "BPatch.h"
 #include "BPatch_addressSpace.h"
 #include "BPatch_process.h"
@@ -6,10 +7,20 @@
 #include "BPatch_point.h"
 #include "BPatch_function.h"
 #include "BPatch_flowGraph.h"
+#include "Instruction.h"
+#include "InstructionDecoder.h"
+#include "slicing.h"
+#include "CodeObject.h"
+#include "CFG.h"
+#include "Graph.h"
 
 using namespace std;
 using namespace Dyninst;
+using namespace InstructionAPI;
+using namespace ParseAPI;
+using namespace DataflowAPI;
 
+/***************************************************************/
 BPatch bpatch;
 
 typedef enum {
@@ -19,110 +30,233 @@ typedef enum {
 } accessType_t;
 
 // Attach, create, or open a file for rewriting
-BPatch_addressSpace* startInstrumenting(accessType_t accessType, const char* name, int pid, const char* argv[]) {
-  BPatch_addressSpace* handle = NULL;
+BPatch_addressSpace *startInstrumenting(accessType_t accessType, const char *name, int pid, const char *argv[]) {
+  BPatch_addressSpace *handle = NULL;
 
-  switch(accessType) {
+  switch (accessType) {
     case create:
       handle = bpatch.processCreate(name, argv);
-      if (!handle) { fprintf(stderr, "processCreate failed\n");}
+      if (!handle) { fprintf(stderr, "processCreate failed\n"); }
       break;
     case attach:
       handle = bpatch.processAttach(name, pid);
-      if (!handle) { fprintf(stderr, "processAttach failed\n");}
+      if (!handle) { fprintf(stderr, "processAttach failed\n"); }
       break;
     case open:
       // Open the binary file and all dependencies
       handle = bpatch.openBinary(name, true);
-      if (!handle) { fprintf(stderr, "openBinary failed\n");}
+      if (!handle) { fprintf(stderr, "openBinary failed\n"); }
       break;
   }
 
   return handle;
 }
 
-void printLineInfo(BPatch_basicBlock *block) {
-    fprintf(stdout, "start %lx end %lx\n", block->getStartAddress(), block->getEndAddress());
-    BPatch_Vector<BPatch_sourceBlock*> sourceBlocks;
-    block->getSourceBlocks(sourceBlocks);
-    for (int i = 0; i < sourceBlocks.size(); i++) {
-        BPatch_Vector<unsigned short> lines;
-        sourceBlocks[i]->getSourceLines(lines);
-        for (int j = 0; j < lines.size(); j++) {
-            fprintf(stdout, "source line: %u.\n", lines[j]);
-        }
-    }
-}
-
-BPatch_basicBlock *getImmediateDominator(BPatch_image* appImage,  const char *func_name, long unsigned int addr) {
-
-    vector<BPatch_function*> functions;
-    appImage->findFunction(func_name, functions);
-    if (functions.size() == 0) {
-        fprintf(stderr, "Loading function %s failed.\n", func_name);
-        return NULL;
-    } else if (functions.size() > 1) {
-        fprintf(stderr, "More than one function with name %s, using one.\n", func_name);
-    }
-
-    BPatch_flowGraph* fg = functions[0]->getCFG();
-
-    set<BPatch_basicBlock*> blocks;
-    fg->getAllBasicBlocks(blocks);
-
-    BPatch_basicBlock *target = NULL;
-    for (auto block_iter = blocks.begin();
-         block_iter != blocks.end();
-         ++block_iter) {
-        BPatch_basicBlock *block = *block_iter;
-        //printLineInfo(block);
-        if (addr >= block->getStartAddress() && addr <= block->getEndAddress()) {
-            target = block;
-            break;
-        }
-    }
-
-    if (target == NULL) {
-        fprintf(stderr, "Failed to find basic block for function %s @ %lx.\n", func_name, addr);
-        return NULL;
-    }
-    return target->getImmediateDominator();
-}
-
-InstructionAPI::Instruction findIfCondition(BPatch_basicBlock *block) {
-  std::vector<InstructionAPI::Instruction> insns;
-  block->getInstructions(insns);
-  InstructionAPI::Instruction ret = *insns.rbegin();
-  assert(ret.getCategory() == InstructionAPI::InsnCategory::c_BranchInsn);
-  return ret;
-}
-
-int main() {
-  // Set up information about the program to be instrumented 
-  const char* progName = "909_ziptest_exe";
+//TODO refactor and put in a separate file with proper include n stuff...
+BPatch_image *getImage(const char *progName) {
   int progPID = 42;
-  const char* progArgv[] = {"test.zip"};
+  const char *progArgv[] = {"test.zip"};
   accessType_t mode = open;
 
   // Just open the binary for analysis, doesn't actually instrument it!
-  BPatch_addressSpace* app = startInstrumenting(mode, progName, progPID, progArgv);
+  BPatch_addressSpace *app = startInstrumenting(mode, progName, progPID, progArgv);
   if (!app) {
-  	fprintf(stderr, "opening the binary failed\n");
-	exit(1);
+    fprintf(stderr, "opening the binary failed\n");
+    exit(1);
   }
 
-  BPatch_image* appImage = app->getImage();
+  BPatch_image *appImage = app->getImage();
+  return appImage;
+}
 
-  // TODO, find the right address for the starting line ....
-  BPatch_basicBlock * immed_dom = getImmediateDominator(appImage, "scanblock", 0x4094ef);
+void printLineInfo(BPatch_basicBlock *block) {
+  fprintf(stdout, "start %lx end %lx\n", block->getStartAddress(), block->getEndAddress());
+  BPatch_Vector < BPatch_sourceBlock * > sourceBlocks;
+  block->getSourceBlocks(sourceBlocks);
+  for (int i = 0; i < sourceBlocks.size(); i++) {
+    BPatch_Vector<unsigned short> lines;
+    sourceBlocks[i]->getSourceLines(lines);
+    for (int j = 0; j < lines.size(); j++) {
+      fprintf(stdout, "source line: %u.\n", lines[j]);
+    }
+  }
+}
 
-  InstructionAPI::Instruction ifCond = findIfCondition(immed_dom);
+void printAddrToLineMappings(BPatch_image *appImage, const char *funcName) {
 
-  // find operands of an instruction, decide how to handle!
-  // => find the operator type
-  // => trace dataflow, find definition point?
+  vector < BPatch_function * > functions;
+  appImage->findFunction(funcName, functions);
+  if (functions.size() == 0) {
+    fprintf(stderr, "Loading function %s failed.\n", funcName);
+    return;
+  } else if (functions.size() > 1) {
+    fprintf(stderr, "More than one function with name %s, using one.\n", funcName);
+  }
 
-  // how to handle when there are multiple variables used in an instruction?
+  BPatch_flowGraph *fg = functions[0]->getCFG();
+
+  set <BPatch_basicBlock *> blocks;
+  fg->getAllBasicBlocks(blocks);
+
+  for (auto blockIter = blocks.begin();
+       blockIter != blocks.end();
+       ++blockIter) {
+    BPatch_basicBlock *block = *blockIter;
+    printLineInfo(block);
+  }
+}
+
+BPatch_basicBlock *getImmediateDominator(BPatch_image *appImage, const char *funcName, long unsigned int addr) {
+
+  vector < BPatch_function * > functions;
+  appImage->findFunction(funcName, functions);
+  if (functions.size() == 0) {
+    fprintf(stderr, "Loading function %s failed.\n", funcName);
+    return NULL;
+  } else if (functions.size() > 1) {
+    fprintf(stderr, "More than one function with name %s, using one.\n", funcName);
+  }
+
+  BPatch_flowGraph *fg = functions[0]->getCFG();
+
+  set <BPatch_basicBlock *> blocks;
+  fg->getAllBasicBlocks(blocks);
+
+  BPatch_basicBlock *target = NULL;
+  for (auto blockIter = blocks.begin();
+       blockIter != blocks.end();
+       ++blockIter) {
+    BPatch_basicBlock *block = *blockIter;
+    printLineInfo(block);
+    if (addr >= block->getStartAddress() && addr <= block->getEndAddress()) { //TODO inclusive?
+      target = block;
+      break;
+    }
+  }
+
+  if (target == NULL) {
+    fprintf(stderr, "Failed to find basic block for function %s @ %lx.\n", funcName, addr);
+    return NULL;
+  }
+  return target->getImmediateDominator();
+}
+
+Instruction findIfCondition(BPatch_basicBlock *block) {
+  vector <Instruction> insns;
+  block->getInstructions(insns);
+  Instruction ret = *insns.rbegin();
+  assert(ret.getCategory() == InsnCategory::c_BranchInsn);
+  return ret;
+}
+/***************************************************************/
+
+Function *getFunction(const char *binaryPath, const char *funcName) {
+  SymtabAPI::Symtab *symTab;
+  string binaryPathStr(binaryPath);
+  string funcNameStr(funcName);
+  bool isParsable = SymtabAPI::Symtab::openFile(symTab, binaryPathStr);
+  if (isParsable == false) {
+    fprintf(stderr, "File cannot be parsed: %s.\n", binaryPath);
+    return NULL;
+  }
+
+  SymtabCodeSource *sts = new SymtabCodeSource((char *)binaryPath);
+  CodeObject *co = new CodeObject(sts);
+
+  co->parse();
+
+  const CodeObject::funclist &all = co->funcs();
+  if (all.size() == 0) {
+    fprintf(stderr, "No function in file %s.\n", binaryPath);
+    return NULL;
+  }
+
+  for (auto fit = all.begin(); fit != all.end(); ++fit) {
+    Function *f = *fit;
+    if (f->name().compare(funcNameStr) == 0) {
+      return f;
+    }
+  }
+  return NULL;
+}
+
+Block *getImmediateDominator2(Function *f, long unsigned int addr) {
+  Block *target = NULL;
+  for (auto bit = f->blocks().begin(); bit != f->blocks().end(); ++bit) {
+    Block *b = *bit;
+    if (addr >= b->start() && addr < b->end()) { // "Address immediately following the last instruction in the block."
+      target = b;
+      break;
+    }
+  }
+
+  if (target == NULL) {
+    cerr << "Failed to find basic block for function" << f->name() << " @ " << addr << endl;
+    return NULL;
+  }
+  return f->getImmediateDominator(target);
+}
+
+Instruction findIfCondition2(Block *b) {
+  // Decode the instruction
+  const unsigned char *buf = (const unsigned char *) b->obj()->cs()->getPtrToInstruction(b->last());
+  InstructionDecoder dec(buf, InstructionDecoder::maxInstructionLength, b->obj()->cs()->getArch());
+  Instruction ret = dec.decode();
+  assert(ret.getCategory() == InsnCategory::c_BranchInsn);
+  return ret;
+}
+
+class CustomSlicer : public Slicer::Predicates {
+public:
+  int i = 0;
+  virtual bool endAtPoint(Assignment::Ptr ap) {
+    i = i + 1;
+    if (i > 8) return true;
+    return false;
+    //return ap->insn().readsMemory();
+  }
+
+  virtual bool addPredecessor(AbsRegion reg) {
+    return true;
+  }
+};
+
+void slice(Function *f, Block *b, Instruction insn) {
+
+  // Convert the instruction to assignments
+  AssignmentConverter ac(true, true);
+  vector<Assignment::Ptr> assignments;
+  ac.convert(insn, b->last(), f, b, assignments);
+
+  // An instruction can corresponds to multiple assignments
+  Assignment::Ptr assign; //TODO, how to handle multiple ones?
+  for (auto ait = assignments.begin(); ait != assignments.end(); ++ait) {
+    cout << (*ait)->format() << endl;
+    assign = *ait;
+  }
+
+  Slicer s(assign, b, f);
+  CustomSlicer cs;
+  GraphPtr slice = s.backwardSlice(cs);
+  cout << slice->size() << endl;
+  string filePath("/home/anygroup/perf_debug_tool/binary_analysis/graph");
+  slice->printDOT(filePath);
+}
 
 
+int main() {
+  // Set up information about the program to be instrumented 
+  const char *progName = "909_ziptest_exe";
+  const char *funcName = "scanblock";
+
+  //BPatch_image *appImage = getImage(progName);
+  //printAddrToLineMappings(appImage, funcName);
+  //BPatch_basicBlock *immedDom = getImmediateDominator(appImage, funcName, 0x40940c);
+  //Instruction ifCond = findIfCondition(immedDom);
+  /***************************************************************/
+
+  Function *func = getFunction(progName, funcName);
+  Block *immedDom = getImmediateDominator2(func, 0x40940c);
+  Instruction ifCond = findIfCondition2(immedDom);
+  slice(func, immedDom, ifCond);
 }
