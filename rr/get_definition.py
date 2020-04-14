f856712e581056aa78ea11544a28d2f6ec53ca10
Sitao Wang <sitaowang1998@outlook.com>
2020-04-13 20:53:44 -0400

    Fixed bug of empty output for continue

--- rr/get_definition.py
+++ rr/get_definition.py
@@ -21,13 +21,6 @@ def run(cmd):
     return gdb.execute(cmd, to_string=True).split(os.linesep)
 
 
-def process_exit(outs):
-    for line in outs:
-        if re.search(r'Inferior \d+ \(process \d+\) exited with code \d+', line):
-            return True
-    return False
-
-
 def br_success(outs):
     for line in outs:
         if re.search(r'Make breakpoint pending on future shared library load', line):
@@ -58,13 +51,13 @@ continue_count = 0
 while True:
     continue_count += 1
     try:
-        outs = run('c {}'.format(str(continue_count)))
+        run('c {}'.format(str(continue_count)))
+        outs = run('i reg {}'.format(reg))
     except gdb.error:
         clean_up()
         exit()
-    if process_exit(outs):
+    reg_value = get_reg_value(outs)
+    if reg_value == '':
         clean_up()
         exit()
-    outs = run('i reg {}'.format(reg))
-    reg_value = get_reg_value(outs)
     print(reg_value)
