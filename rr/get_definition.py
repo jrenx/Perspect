fd9bfe776f0d50f56c6be7add06482e5c2da1655
Sitao Wang <sitaowang1998@outlook.com>
2020-04-13 20:27:24 -0400

    Get register value

--- rr/get_definition.py
+++ rr/get_definition.py
@@ -7,16 +7,26 @@ with open('config.json') as configFile:
     config = json.load(configFile)
 
 filename = config['file']
-line = config['line']
+line_num = config['line']
 reg = config['reg']
 if 'out' in config:
     output_filename = config['out']
 else:
     output_filename = 'out.log'
 
+output_file = open(output_filename, 'r')
+
+
 def run(cmd):
-    outs = gdb.execute(cmd, to_string=True)
-    return outs.split(os.linesep)
+    return gdb.execute(cmd, to_string=True).split(os.linesep)
+
+
+def process_exit(outs):
+    for line in outs:
+        if re.search(r'Inferior \d+ \(process \d+\) exited with code \d+', line):
+            return True
+    return False
+
 
 def br_success(outs):
     for line in outs:
@@ -24,7 +34,37 @@ def br_success(outs):
             return False
     return True
 
-outs = run('br {}:{}'.format(filename, line))
+
+def get_reg_value(outs):
+    for line in outs:
+        words = line.split()
+        if len(words) >= 2 && words[0] == reg:
+            return words[1]
+    return ''
+
+
+def clean_up():
+    output_file.close()
+
+
+outs = run('br {}:{}'.format(filename, str(line_num)))
 if not br_success(outs):
     print('Setting breakpoints failed')
     exit(1)
+
+run('r')
+
+continue_count = 0
+while True:
+    continue_count += 1
+    try:
+        outs = run('c {}'.format(str(continue_count)))
+    except gdb.error:
+        clean_up()
+        exit()
+    if process_exit(outs):
+        clean_up()
+        exit()
+    outs = run('i reg {}'.format(reg))
+    reg_value = get_reg_value(outs)
+    print(reg_value)
