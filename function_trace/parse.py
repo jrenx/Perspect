cddc9179b8c297b06954b70135fe524aa3146503
Sitao Wang <sitaowang1998@outlook.com>
2020-04-29 20:39:06 -0400

    Modified to use class for use in cpp

--- function_trace/parse.py
+++ function_trace/parse.py
@@ -1,31 +1,64 @@
 import sys
+import subprocess
+import os
 
-def parse_function_trace(filename):
 
-    traces = []
+class TraceCollector:
 
-    with open(filename, 'r') as inputFile:
-        trace = []
-        for line in inputFile:
-            if 'start' in line:
-                if len(trace) > 0:
-                    traces.append(trace)
-                trace = []
-            elif 'eof' in line:
-                if len(trace) > 0:
-                    traces.append(trace)
-                break
-            elif '0x' in line:
-                trace.append(int(line, 16))
-            else:
-                print("Warning: failed to parse line: " + line)
+    def __init__(self, program, is_32=False, pin='pin'):
+        # program is a string that can run the test program
+        self.program = program.split()
+        self.is_32 = is_32
+        self.pin = pin  # path to pin binary
+        self.traces = {}
 
-    return traces
+    def is_trace_available(self, fucntion_name):
+        return fucntion_name in self.traces
 
-# main for testing
-if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        filename = sys.argv[1]
-    else:
-        filename = 'ftrace.out'
-    print(len(parse_function_trace(filename)))
+    def is_trace_on_disk(self, function_name):
+        return os.path.isfile('{}.out'.format(function_name))
+
+    def read_trace_from_disk(self, function_name):
+        self.traces[function_name] = self.parse_function_trace('{}.out'.format(function_name))
+
+    def run_function_trace(self, function_name):
+        if self.is_32:
+            obj_file = os.path.join('obj-ia32', 'function_trace.so')
+        else:
+            obj_file = os.path.join('obj-intel64', 'function_trace.so')
+        pin_program_list = [self.pin, '-t', obj_file, '-f', function_name, '-o', '{}.out'.format(function_name), '--']
+        pin_program_list.extend(self.program)
+        subprocess.run(pin_program_list, shell=True, check=True)
+
+    def is_instruction_after(self, function_name, before, after):
+        traces = self.traces[function_name]
+        for trace in traces:
+            found = False
+            for instruction in trace:
+                if found and instruction != after:
+                    return False
+                if instruction == before:
+                    found = True
+                else:
+                    found = False
+
+    def parse_function_trace(self, filename):
+        traces = []
+
+        with open(filename, 'r') as inputFile:
+            trace = []
+            for line in inputFile:
+                if 'start' in line:
+                    if len(trace) > 0:
+                        traces.append(trace)
+                    trace = []
+                elif 'eof' in line:
+                    if len(trace) > 0:
+                        traces.append(trace)
+                    break
+                elif '0x' in line:
+                    trace.append(int(line, 16))
+                else:
+                    print("Warning: failed to parse line: " + line)
+
+        return traces
