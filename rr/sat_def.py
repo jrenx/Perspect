bca6b098920495b39b6674041b8e66c2abf6add4
Sitao Wang <sitaowang1998@outlook.com>
2020-05-08 21:19:36 -0400

    Added fix for str to byte

--- rr/sat_def.py
+++ rr/sat_def.py
@@ -7,7 +7,7 @@ def run_break_points(breakpoints):
     json.dump({"breakpoints": breakpoints}, open('config.json', 'w'))
     rr_process = subprocess.Popen('sudo rr replay', stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
     try:
-        rr_process.communicate('source get_breakpoints', 60 * 10)
+        rr_process.communicate('source get_breakpoints'.encode(), 60 * 10)
     except subprocess.TimeoutExpired:
         rr_process.kill()
         return False
