0627ac85589c37100d0b09c2d940764d3b792e09
Xiang Ren <jenny.ren@mail.utoronto.ca>
2020-05-20 13:27:08 -0400

    Added fix to adding HARDCODED working directory in pin python files.
    Former-commit-id: 3bdb751767a5378cc39916a1f3e5b857bf81bcf9

--- function_trace/instruction_reg_trace.py
+++ function_trace/instruction_reg_trace.py
@@ -12,9 +12,9 @@ class InsRegTrace:
 
     def run_function_trace(self, ins_reg_map):
         if self.is_32:
-            obj_file = os.path.join('obj-ia32', working_dir + 'instruction_reg_log.so')
+            obj_file = os.path.join(working_dir, 'obj-ia32', 'instruction_reg_log.so')
         else:
-            obj_file = os.path.join('obj-intel64', working_dir + 'instruction_reg_log.so')
+            obj_file = os.path.join(working_dir, 'obj-intel64', 'instruction_reg_log.so')
         pin_program_list = [self.pin, '-t', obj_file, '-o', working_dir + 'instruction_trace.out']
 
         for ins, reg in ins_reg_map.items():
