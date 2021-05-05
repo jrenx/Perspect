8536b70858997c9c17bc83e3aeb2a1dd06f228b8
Xiang Ren <jenny.ren@mail.utoronto.ca>
2021-05-05 03:56:27 -0400

    Debugged parallelizing script
    Former-commit-id: 6a62e2d0f4f9011a1478c8f0ee7d1c9e5f82f454

--- parallelize.bash
+++ parallelize.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
-dst= $HOME"/rr_runs/"
 src=$PWD
 folder=${PWD##*/}
+dst=$HOME"/"$folder"_rr_runs/"
+echo "source: "$src
+echo "destination: "$dst
 mkdir $dst
 cp -r $PWD $dst
 cp $src"/parallelize/combine.py" $dst
@@ -9,18 +11,21 @@ cp $src"/parallelize/cp_script" $dst
 cp $src"/parallelize/rename.py" $dst
 cp $src"/parallelize/run" $dst
 cp $src"/parallelize/run-rr.py" $dst$folder
-cp $src"/parallelize/rr_util.py.record_rr_inputs" $src"/rr_util.py/"
+cp $src"/parallelize/rr_util.py.record_rr_inputs" $src"/rr_util.py"
 cd $dst
+echo $PWD
 ./cp_script $folder
 for (( i=1; i<=1000; i++ ))
-do  
+do
   echo "Running at iteration "$i
-  cd $src#/home/anygroup/eval_909_32bit
+  echo "source: "$src
+  cd $src
+  echo $PWD
   python3 static_dep_graph.py >> out
   mv rr_inputs $dst
   cd $dst
   ./run $folder
   python3 combine.py $folder $src $1
   python3 rename.py $i $folder
-  cp $dst"rr_results.json" $src"/cache/rr_results"$1".json" #the program name
+  cp $dst"rr_results.json" $src"/cache/rr_results_"$1".json" #the program name
 done
--- parallelize/rename.py
+++ parallelize/rename.py
@@ -6,11 +6,11 @@ curr_dir = os.path.dirname(os.path.realpath(__file__))
 
 def run():
     c = int(sys.argv[1])
-    folder = int(sys.argv[2])
+    folder = sys.argv[2]
     rr_result_file = os.path.join(curr_dir, 'rr_results.json')
     for i in range(0,16):
         f1 = os.path.join(curr_dir, folder + "_" + str(i), "cache", "rr_results_" + str(i) + ".json")
-        f2 = os.path.join(curr_dir, folder + "_" + str(i), "cache", "rr_results_" + str(i) + "_" + str(c) + ".json"
+        f2 = os.path.join(curr_dir, folder + "_" + str(i), "cache", "rr_results_" + str(i) + "_" + str(c) + ".json")
         subprocess.run(["mv", f1, f2])
         f1 = os.path.join(curr_dir, folder + "_" + str(i), "out")
         f2 = os.path.join(curr_dir, folder + "_" + str(i), "out" + str(c))
