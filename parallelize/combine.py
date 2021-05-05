689fb52adb169332782a9c0e46a538eaedc9e9df
Xiang Ren <jenny.ren@mail.utoronto.ca>
2021-05-05 14:37:47 -0400

    Part 7 of the fix.
    Former-commit-id: faaf9cd65381e0acbcf1118386722766389b752e

--- parallelize/combine.py
+++ parallelize/combine.py
@@ -13,15 +13,22 @@ def run():
     rr_result_cache = {}
     rr_result_file = os.path.join(curr_dir, 'rr_results.json')
     for i in range(0,16):
-        with open(os.path.join(curr_dir, folder + "_" + str(i), "cache", "rr_results_" + str(i) + ".json"), "r") as f:
-            rr_result_cache.update(json.load(f))
-            print(i)
-            print(len(rr_result_cache))
+        curr_rr_result_file = os.path.join(curr_dir, folder + "_" + str(i), "cache", "rr_results_" + str(i) + ".json")
+        if os.path.exists(curr_rr_result_file):
+            with open(curr_rr_result_file, "r") as f:
+                rr_result_cache.update(json.load(f))
+                print(i)
+                print(len(rr_result_cache))
+        else:
+            print("RR file not present at " + str(i))
+
     rr_src_result_file = os.path.join(src, "cache", "rr_results_" + prog + ".json")
     if os.path.exists(rr_src_result_file):
         with open(rr_src_result_file, "r") as f:
             rr_result_cache.update(json.load(f))
             print(len(rr_result_cache))
+    else:
+        print("Source RR file does not exist")
    
     print("Persisting rr result file")
     with open(rr_result_file, 'w') as f:
