5b45349bbb377aeda1ffff67eaedd02a12debbc7
Sitao Wang <sitaowang1998@outlook.com>
2021-07-04 20:04:51 -0400

    Simplified watchpoint
    Former-commit-id: e81487cb0346c48c396ecfe4c2b2536433e07fc2

--- rr/get_def.py
+++ rr/get_def.py
@@ -351,7 +351,7 @@ def get_def(prog, branch, target, read, reg, shift='0x0', offset='0x0', offset_r
         # Second pass
         print("[rr] Running second step for {} times".format(i + 1), flush=True)
         run_watchpoint([], watchpoints)
-        watchpoint_trace = parse_watchpoint([], watchpoints, read)
+        watchpoint_trace = parse_watchpoint()
         print("[rr] Parsed " + str(len(watchpoint_trace)) + " watchpoint hits")
         #("[tmp] " + str(watchpoint_trace))
         print("[rr] Second step finished")
--- rr/watchpoints.py
+++ rr/watchpoints.py
@@ -1,97 +1,47 @@
-import gdb
-import json
-import re
 import os
+import sys
+import json
+
+import gdb
 
 rr_dir = os.path.dirname(os.path.realpath(__file__))
 
-class InitArgument(gdb.Function):
-    def __init__(self):
-        super(InitArgument, self).__init__('init_argument')
+with open(os.path.join(rr_dir, 'config.json')) as config_file:
+    config = json.load(config_file)
 
-    def invoke(self):
-        with open(os.path.join(rr_dir, 'config.json')) as configFile:
-            config = json.load(configFile)
+breakpoints = config['breakpoints']
+watchpoints = config['watchpoints']
 
-        breakpoints = config['breakpoints']
-        watchpoints = config['watchpoints']
-        rwatchpoints = config['rwatchpoints']
-        #print(watchpoints)
-        #print(rwatchpoints)
-        i = 0
-        for wp in watchpoints:
-            print("watch " + str(wp))
-            for i in range(2):
-                if i >= 4:
-                    break
-                cmd = "watch *{}".format(wp)
-                try:
-                    print(cmd)
-                    gdb.execute(cmd)
-                    i += 1
-                    break
-                except Exception as e:  # TODO is there a more specific error?
-                    print(str(e))
-                    print("Failed to set watchpoint at " + str(wp))
-                    cmd = "watch -l *(long *){}".format(wp)
+for wp in watchpoints:
+    print("watch " + str(wp))
+    try:
+        gdb.execute('watch *{}'.format(wp))
+    except Exception:
+        try:
+            gdb.execute('watch *(long *){}'.format(wp))
+        except Exception:
+            print('[Error] Failed to set watchpoint {}'.format(wp), file=sys.stderr)
 
-        for wp in rwatchpoints:
-            print("rwatch " + str(wp))
-            for i in range(2):
-                if i >= 4:
-                    break
-                cmd = "rwatch *{}".format(wp)
-                try:
-                    print(cmd)
-                    gdb.execute(cmd)
-                    i += 1
-                    break
-                except Exception as e:  # TODO is there a more specific error?
-                    print(str(e))
-                    print("Failed to set rwatchpoint at " + str(wp))
-                    cmd = "rwatch -l *(long *){}".format(wp)
+for br in breakpoints:
+    gdb.execute("br {}".format(br))
 
-        for br in breakpoints:
-            gdb.execute("br {}".format(br))
+breakpoint_values = [int(br.strip('*'), base=16) for br in breakpoints]
+watchpoint_values = [int(wp.strip('*'), base=16) for wp in watchpoints]
 
-        return 1
+trace = []
 
+while True:
+    try:
+        gdb.execute('c')
+        frame = gdb.newest_frame()
+    except Exception:
+        break # End of process
+    pc = int(frame.pc())
+    if pc in watchpoints:
+        func = str(frame.name())
+        trace.append((watchpoints[watchpoint_values.index(pc)], hex(pc), func))
+    else:
+        print('[Error] gdb stop at unkown point {}'.format(hex(pc)), file=sys.stderr)
 
-InitArgument()
-
-
-class CheckProcessExit(gdb.Function):
-    def __init__(self):
-        super(CheckProcessExit, self).__init__('is_process_exit')
-
-    def invoke(self):
-        with open(os.path.join(rr_dir, 'watchpoints.log'), 'r') as f:
-            position = gdb.convenience_variable('log_position')
-            if position is not None:
-                position = int(position)
-                f.seek(position)
-            outs = f.readlines()
-        for line in outs:
-            if re.search(r'Inferior \d+ \(process \d+\) exited', line):
-                gdb.set_convenience_variable('RET', 1)
-                return 1
-        gdb.set_convenience_variable('RET', 0)
-        return 0
-
-
-CheckProcessExit()
-
-
-class UpdateFilePosition(gdb.Function):
-    def __init__(self):
-        super(UpdateFilePosition, self).__init__('update_file')
-
-    def invoke(self):
-        with open(os.path.join(rr_dir, 'watchpoints.log'), 'r') as f:
-            f.seek(0, 2)
-            position = f.tell()
-        gdb.set_convenience_variable('log_position', position)
-        return 1
-
-
-UpdateFilePosition()
+with open(os.path.join(rr_dir, 'watchpoints.log'), 'w') as log_file:
+    json.dump(trace, log_file)
