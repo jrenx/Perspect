eb8a0d3c2dd56d44585db0b6fbc704fa73caa50d
Sitao Wang <sitao.wang@mail.utoronto.ca>
2021-09-09 17:52:13 -0400

    [parallelize-multiserver] Added more logs and some bug fix
    Former-commit-id: d0ef370c3de5c5105794d5d510d76db6ad38fe12

--- parallelize-client.py
+++ parallelize-client.py
@@ -5,12 +5,12 @@ import select
 import json
 import datetime
 import time
-import traceback
 import socketserver
 import queue
-import _thread
+import threading
 
-worker_addresses = [("10.1.0.21", 12000), ("10.1.0.24", 12000)]
+#worker_addresses = [("10.1.0.21", 12000), ("10.1.0.24", 12000)]
+worker_addresses = [("10.1.0.21", 12000)]
 
 HOST, PORT = "localhost", 9999
 q = queue.Queue()
@@ -23,14 +23,12 @@ class MyTCPHandler(socketserver.StreamRequestHandler):
         # we can now use e.g. readline() instead of raw recv() calls
         self.data = self.rfile.readline().strip().decode("utf-8")
         q.put(self.data)
-        print("{} wrote:".format(self.client_address[0]))
+        #print("{} wrote:".format(self.client_address[0]))
         print(self.data)
 
 def server_thread():
     with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
         server.serve_forever()
-
-
 
 def main():
     curr_dir = os.path.dirname(os.path.realpath(__file__))
@@ -41,46 +39,57 @@ def main():
     rr_result_cache = {}
     handled = set()
 
-    print("Starting execution")
+    server_t = threading.Thread(target=server_thread, args=())
+    server_t.start()
+
+    print("[client] Starting execution", flush=True)
     for i in range(5):
-        print("In iteration {}".format(i))
+        print("In iteration {}".format(i), flush=True)
         start_time = datetime.datetime.now()
         os.system('python3 static_dep_graph.py >> out' + str(i) + ' &')
         if os.path.exists(rr_result_file):
-            with open(rr_result_file) as file:
-                rr_result_cache = json.load(file)
-             print("Execution of iteration {} starts at {}".format(i, datetime.datetime.strftime(start_time, "%Y/%m/%d, %H:%M:%S")))
+            with open(rr_result_file) as f:
+                rr_result_cache = json.load(f)
  
         start_time = datetime.datetime.now()
-        print("Execution of itertaion {} starts at {}".format(i, datetime.datetime.strftime(start_time, "%Y/%m/%d, %H:%M:%S")))
+        print("[client] Execution of itertaion {} starts at {}".format(i, datetime.datetime.strftime(start_time, "%Y/%m/%d, %H:%M:%S")), flush=True)
 
         sockets = []
         for addr in worker_addresses:
-            print(addr)
+            print("[client] Connecting to {}".format(addr), flush=True)
             for _ in range(16):
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect(addr)
                 sockets.append(s)
 
+        print("[client] Sending initial tasks", flush=True)
+        closed_sockets = set()
         for s in sockets:
             while True:
+                print("[client] Waiting for static dep graph results", flush=True)
                 while q.empty():
-                        continue
+                    continue
+                print("[client] Getting result from static dep graph", flush=True)
                 line = q.get()
+                print("[client] Result from static dep graph {}".format(line), flush=True)
 
                 if line.strip().startswith("FIN"):
+                    print("[client] Received FIN from static dep graph", flush=True)
                     q.put(line)
                     s.close()
+                    closed_sockets.add(s)
                     break
 
                 if line not in handled:
                     handled.add(line)
-                    print("Getting key " + line, flush=True)
-                    #print("[sender][" + str(id) + "] Sending task {}".format(line), flush=True)
+                    print("[client] Getting key " + line, flush=True)
+                    print("[client] Sending task {}".format(line), flush=True)
                     s.send(line.encode())
-                    #print("[sender][" + str(id) + "] Sent task {}".format(line), flush=True)
+                    print("[client] Sent task {}".format(line), flush=True)
                     break
+        sockets = [s for s in sockets if s not in closed_sockets]
 
+        print("[client] Waiting for rr results", flush=True)
         while len(sockets) > 0:
             read_sockets, _, _ = select.select(sockets, [], [])
 
@@ -91,7 +100,7 @@ def main():
                     s.close()
                     sockets.remove(s)
                     continue
-                #print("[sender][" + str(id) + "] Receiving result for task {}".format(line), flush=True)
+                print("[client] Receiving result for task {}".format(line), flush=True)
                 ret = json.loads(ret)
                 for (key, value) in ret.items():
                     rr_result_cache[key] = value
@@ -99,26 +108,28 @@ def main():
                 # Send new task if availble
                 while True:
                     while q.empty():
-                            continue
+                        continue
                     line = q.get()
 
                     if line.strip().startswith("FIN"):
+                        print("[client] Received FIN from static dep graph", flush=True)
                         q.put(line)
                         s.close()
+                        socekts.remove(s)
                         break
 
                     if line not in handled:
                         handled.add(line)
-                        print("Getting key " + line, flush=True)
-                        #print("[sender][" + str(id) + "] Sending task {}".format(line), flush=True)
+                        print("[client] Getting key " + line, flush=True)
+                        print("[client] Sending task {}".format(line), flush=True)
                         s.send(line.encode())
-                        #print("[sender][" + str(id) + "] Sent task {}".format(line), flush=True)
+                        print("[client] Sent task {}".format(line), flush=True)
                         break
 
         json.dump(rr_result_cache, open(rr_result_file, 'w'), indent=4)
 
         duration = datetime.datetime.now() - start_time
-        print("Running iteration {} uses {} seconds".format(iter, duration))
+        print("[client] Running iteration {} uses {} seconds".format(iter, duration))
 
 if __name__ == '__main__':
     main()
--- parallelize-worker.py
+++ parallelize-worker.py
@@ -21,8 +21,7 @@ def run_task(id, pipe, prog):
             break
         (prog, a1, a2, a3, a4, a5, a6, a7) = obj
         str_args = '_'.join(map(lambda arg : "None" if arg is None else arg, [prog, a1, a2, a3, a4, a5, a6, a7]))
-        #print("[worker][{}] recive task {}".format(id, str_args),flush=True)
-        print("Process {} recive task {}".format(id, str_args))
+        print("[worker] Process {} recive task {}".format(id, str_args))
         rr_result_cache = {}
         start_time = datetime.datetime.now()
         try:
@@ -35,10 +34,9 @@ def run_task(id, pipe, prog):
             print("-" * 60)
 
         duraton = datetime.datetime.now() - start_time
-        #print("[worker][{}] finish task {} in {}".format(id, str_args, duraton),flush=True)
-        print("Process {} finish task {} in {}".format(id, str_args, duraton))
+        print("[worker] Process {} finish task {} in {}".format(id, str_args, duraton))
         pipe.send(rr_result_cache)
-    #print("[worker][{}] finished execution.".format(id),flush=True)
+    print("[worker] Process {} finished execution.".format(id),flush=True)
     pipe.send("Shutdown")
 
 
@@ -51,7 +49,7 @@ def main():
     if len(sys.argv) > 1:
         prog = sys.argv[1]
 
-    print("Setting up parallel environment")
+    print("[server] Setting up parallel environment")
     for iter in range(num_processor):
         process_dir = os.path.join(curr_dir, 'run_{}'.format(iter))
         if not os.path.exists(process_dir):
@@ -80,7 +78,7 @@ def main():
         processes.append(p)
         pipes.append(parent_conn)
 
-    print("Setting up sockets")
+    print("[server] Setting up sockets")
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listener.bind((socket.gethostname(), port))
     listener.listen(16)
@@ -89,12 +87,12 @@ def main():
 
     while True:
         (client, addr) = listener.accept()
-        print("Getting connection from {}".format(addr))
+        print("[server] Getting connection from {}".format(addr))
 
         def connect_bridge(socket, pipe):
             while True:
                 line = socket.recv(4096).decode()
-                print("Receive from socket: {}".format(line), flush=True)
+                print("[server] Receive from socket: {}".format(line), flush=True)
                 if line == "":
                     pipes.append(pipe)
                     return
@@ -110,13 +108,12 @@ def main():
                 a5 = None if segs[5].strip() == "None" else segs[5].strip() 
                 a6 = None if segs[6].strip() == "None" else segs[6].strip() 
                 a7 = None if segs[7].strip() == "None" else segs[7].strip() 
-                #print("[sender][" + str(id) + "] Sending task {}".format(line), flush=True)
-                print("Send task {}".format(line), flush=True)
+                print("[server] Sending task {}".format(line), flush=True)
                 pipe.send((prog, a1, a2, a3, a4, a5, a6, a7))
-                #print("[sender][" + str(id) + "] Sent task {}".format(line), flush=True)
+                print("[server] Sent task {}".format(line), flush=True)
 
                 ret = pipe.recv()
-                #print("[sender][" + str(id) + "] Receiving result for task {}".format(line), flush=True)
+                print("[server] Receiving result for task {}".format(line), flush=True)
                 socket.send(json.dumps(ret).encode())
 
 
