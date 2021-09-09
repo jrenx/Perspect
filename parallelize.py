35098f8a9d9088c3ec00732ac2256951f8fd69ed
Xiang Ren <jenny.ren@mail.utoronto.ca>
2021-09-09 03:03:26 -0400

    [parallelize] Changed the script to restart another iteration of static slicing not when all tasks have finished but when the total number of pending tasks is low to improve performance. Stop retrying static slicing when the last iteration received 0 new unique inputs.
    Former-commit-id: b71c5fd652035a8457682dcf030fc9a8399e31f4

--- parallelize.py
+++ parallelize.py
@@ -15,21 +15,53 @@ HOST, PORT = "localhost", 9999
 q = queue.Queue()
 DEBUG = True
 num_processor = 16
+num_new_unique_inputs_received = 0
+handled = set()
+
+lock0 = threading.Lock()
+pending_count = 0
+
+lock1 = threading.Lock()
+restart_static_slicing = False
+
+lock2 = threading.Lock()
+# for rr_result_cache
+
+lock3 = threading.Lock()
+it = 0
 
 class MyTCPHandler(socketserver.StreamRequestHandler):
     def handle(self):
         # self.rfile is a file-like object created by the handler;
         # we can now use e.g. readline() instead of raw recv() calls
+        global num_new_unique_inputs_received
         self.data = self.rfile.readline().strip().decode("utf-8")
         if self.data == "FIN":
-            print("Received FIN.")
-            global num_processor
-            for i in range(0, num_processor):
-                q.put(self.data)
+            print("[main receiver] Received FIN.")
+            if num_new_unique_inputs_received > 0:
+                print("[main receiver] Should restart static slicing...")
+                num_new_unique_inputs_received = 0
+                lock1.acquire()
+                global restart_static_slicing
+                restart_static_slicing = True
+                lock1.release()
+            else:
+                print("[main receiver] Did not receive any new unique inputs, finish now...")
+                for i in range(0, num_processor):
+                    q.put(self.data)
         else:
-            q.put(self.data)
-            print("{} wrote:".format(self.client_address[0]))
-            print(self.data)
+            print("[main receiver] " + str(self.client_address[0]) + " wrote:" + str(self.data))
+            line = self.data.strip()
+            if line in handled:
+                print("[main receiver] Ignore input already explored in this iteration: " + line, flush=True)
+                return
+            handled.add(line)
+            q.put(line)
+            num_new_unique_inputs_received += 1
+            lock0.acquire()
+            global pending_count
+            pending_count += 1
+            lock0.release()
 
 def server_thread():
     with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
@@ -42,7 +74,7 @@ def run_task(id, pipe, prog):
     pid = str(os.getpid())
     import rr_util
     import sa_util
-    binary_ptr = setup(prog)
+    binary_ptr = sa_util.setup(prog)
     while True:
         try:
             obj = pipe.recv()
@@ -70,33 +102,42 @@ def run_task(id, pipe, prog):
     print("[worker][{}] finished execution.".format(id),flush=True)
     pipe.send("Shutdown")
 
-def send_task(id, pipe, prog, rr_result_cache, handled, lock):
+def send_task(id, pipe, prog, rr_result_cache, rr_result_file):
     while True:
         try:
             if q.empty():
                 continue
-
-            # line = inputs.pop()
             line = q.get()
             print("[sender][" + str(id) + "] Getting key " + line, flush=True)
-            if line.strip().startswith("FIN"):
+            if line.startswith("FIN"):
                 break
+            lock0.acquire()
+            global pending_count
+            pending_count_local = pending_count
+            lock0.release()
+            print("[sender][" + str(id) + "] pending count is: " + str(pending_count_local))
+            if pending_count_local <= num_processor:
+                restart_static_slicing_local = False
+                lock1.acquire()
+                global restart_static_slicing
+                if restart_static_slicing == True:
+                    restart_static_slicing_local = True
+                    restart_static_slicing = False
+                lock1.release()
+                if restart_static_slicing_local is True:
+                    lock2.acquire()
+                    json.dump(rr_result_cache, open(rr_result_file, 'w'), indent=4)
+                    lock2.release()
 
-            lock.acquire()
-            seen = line in handled
-            if seen is False:
-                handled.add(line)
-            lock.release()
-            if seen is True:
-                print("[sender][" + str(id) + "] Ignore input already explored in this iteration: " + line, flush=True)
-                continue
+                    lock3.acquire()
+                    global it
+                    it += 1
+                    it_local = it
+                    lock3.release()
+                    os.system('python3 static_dep_graph.py --parallelize_rr >> out{} &'.format(it_local))
+                    print("[sender][" + str(id) + "] Execution of static slicing pass {} starts at {}".format(it_local,\
+                        datetime.datetime.strftime(datetime.datetime.now(), "%Y/%m/%d, %H:%M:%S")), flush=True)
 
-            # lock.acquire()
-            # seen = line.strip() in existing_rr_result_cache
-            # lock.release()
-            # if seen is True:
-            #    print("[rr] Ignore input already explored in prev iteration: " + line, flush=True)
-            #    continue
         except IndexError:
             break
         if line.startswith(prog):
@@ -115,9 +156,13 @@ def send_task(id, pipe, prog, rr_result_cache, handled, lock):
         print("[sender][" + str(id) + "] Sent task {}".format(line), flush=True)
         result_cache = pipe.recv()
         print("[sender][" + str(id) + "] Receiving result for task {}".format(line), flush=True)
-        # pending_inputs.remove(line)
+        lock2.acquire()
         for key, value in result_cache.items():
             rr_result_cache[key] = value
+        lock2.release()
+        lock0.acquire()
+        pending_count -= 1
+        lock0.release()
     print("[sender][" + str(id) + "] Thread finishing execution.", flush=True)
     pipe.send("Shutdown")
     while pipe.recv() != "Shutdown":
@@ -131,7 +176,6 @@ def main():
         prog = sys.argv[1]
     rr_result_file = os.path.join(curr_dir, 'cache', prog, 'rr_results_{}.json'.format(prog))
     print("Setting up parallel environment")
-    global num_processor
     for iter in range(num_processor):
         process_dir = os.path.join(curr_dir, 'run_{}'.format(iter))
         if not os.path.exists(process_dir):
@@ -149,87 +193,60 @@ def main():
         shutil.copytree('binary_analysis', binary_dir, ignore=shutil.ignore_patterns('.*', '_*'))
 
     try:
-        _thread.start_new_thread(server_thread, ())
+        t = threading.Thread(target=server_thread, args=())
+        t.start()
     except:
         print("Error: unable to start thread")
 
     print("Starting execution")
-    rr_result_cache = {}
-    handled = set()
-    lock = threading.Lock()
     mp.set_start_method('spawn')
-    exit = False
-    for i in range(5):
-        print("In iteration {}".format(i), flush=True)
-        start_time = datetime.datetime.now()
-        #if not os.path.exists("rr_inputs"):
-        os.system('python3 static_dep_graph.py >> out' + str(i) + ' &')
-        #existing_rr_result_cache = None
-        if os.path.exists(rr_result_file):
-            with open(rr_result_file) as file:
-                rr_result_cache = json.load(file)
-        #inputs = set()
-        #for line in open('rr_inputs', 'r').readlines():
-        #    if existing_rr_result_cache is not None:
-        #        if line.strip() in existing_rr_result_cache:
-        #            print("[rr] Ignore input already explained: " + line)
-        #            continue
-        #    if line in inputs:
-        #        print("[rr] Ignore duplicate input: " + line)
-        #        continue
-        #    inputs.add(line)
-        #inputs = list(inputs) #Remove duplicates
-        #pending_inputs = set(inputs)
-        #print("Static dep graph took: {}".format(datetime.datetime.now() - start_time), flush=True)
-        #print("Static dep graph produced {} non-duplicate inputs".format(len(inputs)), flush=True)
 
-        #start_time = datetime.datetime.now()
-        print("Execution of iteration {} starts at {}".format(i, datetime.datetime.strftime(start_time, "%Y/%m/%d, %H:%M:%S")))
-        processes = []
-        threads = []
-        try:
+    start_time = datetime.datetime.now()
+    os.system('python3 static_dep_graph.py --parallelize_rr >> out0 &')
+    print("Execution of static slicing pass 0 starts at {}".format( \
+        datetime.datetime.strftime(datetime.datetime.now(), "%Y/%m/%d, %H:%M:%S")), flush=True)
+    rr_result_cache = {}
+    if os.path.exists(rr_result_file):
+        with open(rr_result_file) as file:
+            rr_result_cache = json.load(file)
 
-            for j in range(num_processor):
-                parent_conn, child_conn = mp.Pipe(duplex=True)
-                p = mp.Process(target = run_task, args=(j, child_conn, prog))
-                p.start()
-                processes.append(p)
-                t = threading.Thread(target=send_task, args=(j, parent_conn, prog, rr_result_cache, handled, lock))
-                t.start()
-                threads.append(t)
+    processes = []
+    threads = []
+    try:
+        for j in range(num_processor):
+            parent_conn, child_conn = mp.Pipe(duplex=True)
+            p = mp.Process(target = run_task, args=(j, child_conn, prog))
+            p.start()
+            processes.append(p)
+            t = threading.Thread(target=send_task, args=(j, parent_conn, prog, rr_result_cache, rr_result_file))
+            t.start()
+            threads.append(t)
 
-            for j in range(num_processor):
-                processes[j].join()
-                threads[j].join()
-        except Exception as e:
-            print("Running parallelized RR failed")
-            print(str(e))
-            print("-" * 60)
-            traceback.print_exc(file=sys.stdout)
-            print("-" * 60)
-        except KeyboardInterrupt:
-            print('Interrupted')
-            exit = True
+        for j in range(num_processor):
+            processes[j].join()
+            threads[j].join()
+    except Exception as e:
+        print("Running parallelized RR failed")
+        print(str(e))
+        print("-" * 60)
+        traceback.print_exc(file=sys.stdout)
+        print("-" * 60)
+    except KeyboardInterrupt:
+        print('Interrupted')
 
-        json.dump(rr_result_cache, open(rr_result_file, 'w'), indent=4)
+    json.dump(rr_result_cache, open(rr_result_file, 'w'), indent=4)
 
-        #if DEBUG is True:
-        #    timestamp = str(time.time())
-        #    print("[rr] renaming to " + 'rr_inputs' + '.' + timestamp)
-        #    os.rename('rr_inputs', 'rr_inputs' + '.' + timestamp)
-        #else:
-        #    os.system('rm rr_inputs')
-        #
-        #if len(pending_inputs) > 0:
-        #    print("[rr] Did not finish processing all inputs, writing back: " + str(len(inputs)))
-        #    with open('rr_inputs', 'w') as f:
-        #        for line in pending_inputs:
-        #            f.write(line)
+    lock3.acquire()
+    global it
+    it += 1
+    it_local = it
+    lock3.release()
+    os.system('python3 static_dep_graph.py >> out{}'.format(it_local))
+    print("Execution of static slicing pass {} starts at {}".format(it_local, \
+            datetime.datetime.strftime(datetime.datetime.now(), "%Y/%m/%d, %H:%M:%S")), flush=True)
 
-        duration = datetime.datetime.now() - start_time
-        print("Running iteration {} uses {} seconds".format(i, duration))
-        if exit is True:
-            break
+    duration = datetime.datetime.now() - start_time
+    print("Running static slicing with parallized RR took {} ".format(duration))
 
 if __name__ == '__main__':
     main()
