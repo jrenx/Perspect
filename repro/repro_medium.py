6f202e8ea9b21314ac24490a938c37bc90c66900
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-10-28 15:26:51 -0400

    [mongod-44991] Made a medium length repro such that pin can produce a relatively smaller trace in 15min, it is 20x shorter than the original repro.
    Former-commit-id: e325b01860ab140f087d71e96329ae4d0ac9efd0

---
+++ repro/repro_medium.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import multiprocessing
+import time
+import pymongo
+from pymongo import MongoClient
+import json
+# start mongo server
+def start(mongo_dir="~/mongoDB/4.2.1"):
+    db = mongo_dir + "/db"
+    mongod = mongo_dir + "/bin/mongod"
+    log = mongo_dir + "/db.log"
+    os.system("{} --dbpath {} --logpath {} --wiredTigerCacheSizeGB 10 --fork".format(mongod, db, log))
+
+def update(idd):
+    mod = 100
+    
+    client = MongoClient()
+    db = client['database']
+    #print(db.command("dbstats"))
+    #s = db.command("collstats", "c"+str(i))
+    #print(s)
+    c = db['c{}'.format(idd)]
+    a = time.time()
+    for i in range(1):
+        c.update_many({'_id': {'$mod': [mod, i % mod]}}, {'$inc': {'a': 1}})    
+    b = time.time()
+    print("Took " + str(b-a))
+
+if __name__ == '__main__':
+
+    nthreads = 5
+    threads = []
+    for i in range(nthreads):
+        t = multiprocessing.Process(target=update, args=(i,))
+        threads.append(t)
+        t.start()
+    for i in range(nthreads):
+        threads[i].join()
