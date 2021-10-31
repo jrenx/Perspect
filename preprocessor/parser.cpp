4e60c858d29d2a46b7b2c30f97ad38e4ee8ea36b
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-10-30 22:20:21 -0400

    [preprocess/multithreading] Parse 1 byte thread id in refactored logic.
    Former-commit-id: 0010d1281bfab8620e9f54d63963da892f4f8b8d

--- preprocessor/parser.cpp
+++ preprocessor/parser.cpp
@@ -574,7 +574,7 @@ public:
     // Note: the same instruction executed will have multiple UIDs if multiple regs are printed at the instrustion
     unsigned short code;
     long regValue;
-    int threadId;
+    u_int8_t threadId;
     StaticNode *sn;
     bool loadsMemory;
     int regCount2;
@@ -589,8 +589,8 @@ public:
       assert(code <= CodeCount);
       assert(code >= 0);
       if (code == 0) {
-        i -= 4;
-        std::memcpy(&threadId, buffer + i, sizeof(int));
+        i -= 1;
+        std::memcpy(&threadId, buffer + i, sizeof(u_int8_t));
         continue;
       }
 
