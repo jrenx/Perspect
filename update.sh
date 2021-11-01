15a618bfdabfb6cf6a114383e128dde7deb2c78f
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-11-01 17:38:38 -0400

    [server management] Part 2 of Scripts to make updating each servers code and restarting easier.
    Former-commit-id: 5328e67379f7d225ceb86a27dc3a52db485438da

--- update.sh
+++ update.sh
@@ -2,10 +2,4 @@ cd /home/renxian2/eval_mongodb_44991
 pkill python3 -u renxian2
 pkill para -u renxian2
 git pull
-cd /home/renxian2/eval_mongodb_44991/preprocessor
-make clean
-make
-cd ..
-cd /home/renxian2/eval_mongodb_44991/pin
-make PIN_ROOT=~/pin-3.11
 exit
