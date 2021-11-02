89808ab1255fe01e64fa411eb35e4ac3b7038eb1
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-11-02 11:07:41 -0400

    [server management] Separate update and kill into 2 scripts.
    Former-commit-id: 67c4631a28358f90b8164a89ecfd149cdf3f869d

---
+++ kill_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for ip in 10.1.0.17 10.1.0.18 10.1.0.19 10.1.0.20 10.1.0.21 10.1.0.22 10.1.0.23 10.1.0.24
+do
+    echo "Updating "$ip
+    ssh $ip /home/renxian2/eval_mongodb_44991/kill.sh
+done
+
+
--- update.sh
+++ update.sh
@@ -1,5 +1,3 @@
 cd /home/renxian2/eval_mongodb_44991
-pkill python3 -u renxian2
-pkill para -u renxian2
 git pull
 exit
