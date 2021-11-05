ca1dbfeea675048bb7046c1cf4e0c30a13d05fb6
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-11-05 17:14:22 -0400

    [server_management] Fix for Got remote compile to work.
    Former-commit-id: 807f4e9d7f0bc9f607027568b0ec79bbd06d718d

--- compile_all.sh
+++ compile_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "WARN: you need to comment out the code section under '# If not running interactively, don't do anything' in beginning of ~/.bashrc in order for this script to work."
 echo "Current dir is: "$SDIR
 ips=()
 while read -r line; do
@@ -9,5 +10,5 @@ done < servers.config
 for ip in "${ips[@]}"
 do
     echo "Updating "$ip
-    ssh $ip source ~/.bashrc; source ~/.profile; $SDIR"/"compile.sh
+    ssh $ip $SDIR"/"compile.sh
 done
