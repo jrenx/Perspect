71966539e24a8e4b38a286d6c28ef5b7f033b73a
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-11-05 19:12:52 -0400

    [server management] Logging improvement.
    Former-commit-id: 0c92ddbb118e790aaa54f06f3dc53e78c6b5b8a6

--- slice.sh
+++ slice.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./spawn_all.sh "python3.7 parallelize-worker.py" "worker.log"
+echo "Waiting "$1" seconds for the workers to become ready"
 sleep $1
 python3.7 parallelize-client.py &> client.log
