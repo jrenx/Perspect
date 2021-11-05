04f47599f41440f2b2164a942042d57201f2c6dd
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-11-05 18:48:16 -0400

    [server management] Added a script to spawn all workers.
    Former-commit-id: 20cd97a543ff8b541d24a40f69437279e2493736

---
+++ spawn_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "usage example: ./spawn_all.sh  "'"'"python3.7 ra-worker.py"'"'" "'"'"ra_worker.log"'"'
+SDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Current dir is: "$SDIR
+ips=()
+while read -r line; do
+    ips+=($line)
+done < servers.config
+
+for ip in "${ips[@]}"
+do
+    echo "Starting on "$ip
+    cmd="cd "$SDIR"; nohup "$1" > "$2" 2>&1 &"
+    echo $cmd
+    ssh $ip $cmd
+done
