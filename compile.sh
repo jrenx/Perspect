b956837b8791e35380d56cd05bd0389ea2ad2460
renxian2 <renxian2@baker21.dsrg.utoronto.ca>
2021-11-05 14:36:55 -0400

    [server_management] Got remote compile to work.
    Former-commit-id: b13694b017aae302200ab2e2a07ad829490f41ca

--- compile.sh
+++ compile.sh
@@ -1,9 +1,8 @@
-cd /home/renxian2/eval_mongodb_44991/binary_analysis
-make clean
-make
-cd /home/renxian2/eval_mongodb_44991/preprocessor
-make clean
-make
-cd /home/renxian2/eval_mongodb_44991/pin
-make clean
-make PIN_ROOT=/home/renxian2/pin-3.11
+SDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Current dir is: "$SDIR
+cd $SDIR"/"binary_analysis && make clean
+cd $SDIR"/"binary_analysis && make
+cd $SDIR"/"preprocessor && make clean
+cd $SDIR"/"preprocessor && make
+cd $SDIR"/"pin && make clean
+cd $SDIR"/"pin && make PIN_ROOT=/home/renxian2/pin-3.11
