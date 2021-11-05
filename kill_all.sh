d8b02dbdb7e2bc07a68f4af1a6b268cca0fd0c7f
renxian2 <renxian2@baker21.dsrg.utoronto.ca>
2021-11-05 14:11:50 -0400

    [server management] Update script to detect current directory automatically.
    Former-commit-id: 7998ef56689472657f3999da271f808cefbba6af

--- kill_all.sh
+++ kill_all.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Current dir is: "$SDIR
 ips=()
 while read -r line; do
     ips+=($line)
@@ -7,7 +9,7 @@ done < servers.config
 for ip in "${ips[@]}"
 do
     echo "Updating "$ip
-    ssh $ip /home/renxian2/eval_mongodb_44991/kill.sh
+    ssh $ip $SDIR"/"kill.sh
 done
 
 
