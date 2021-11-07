2e353da98b949bb001705bda49668c44d117f9e9
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-11-07 15:29:58 -0500

    [indices/server management] Fix and improvements for parallelize addr2line and gdb info line invocations.
    Former-commit-id: 770dc79723b060f869eb76d4c62e3ee77c8db113

--- kill.sh
+++ kill.sh
@@ -1,6 +1,9 @@
+SDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pkill python3 -u $USER
 pkill para -u $USER
 pkill rr -u $USER
 pkill gdb -u $USER
 pkill get_f -u $USER
 pkill get_i -u $USER
+rm $DIR/insns_*
+rm $DIR/file_lines_
