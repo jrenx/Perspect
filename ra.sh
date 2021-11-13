7a59d349b6878284716c4c61299a4b3c3745ea02
renxian2 <renxian2@baker21.dsrg.utoronto.ca>
2021-11-13 01:28:16 -0500

    [ra] Updating the converging script.
    Former-commit-id: e569b1f0adc77985597f75a0f475017d21170293

--- ra.sh
+++ ra.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-./spawn_all.sh "python3.7 ra-worker.py" "ra_worker.log"
+if [ ! -z $3 ] 
+then
+  ./copy_all.sh $3 $4
+fi
+./spawn_all.sh "python3.7 ra-worker.py" "ra_worker.log"${2:-}
 echo "Waiting "$1" seconds for the workers to become ready"
 sleep $1
-python3.7 ra-client.py &> ra_client.log
+python3.7 ra-client.py &> "ra_client.log"${2:-}
