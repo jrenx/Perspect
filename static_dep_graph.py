b51ba30facb6a2a60eff36674e2be14a2419df1a
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-11-14 15:19:59 -0500

    [sg] Fix to properly remove pending node.
    Former-commit-id: 13d856039e628f7bb6eb926a00efae10af2fbce4

--- static_dep_graph.py
+++ static_dep_graph.py
@@ -2023,7 +2023,7 @@ class StaticDepGraph:
                         continue
                     graph.id_to_node[pending_node.id] = pending_node
                     graph.insn_to_node[pending_node.insn] = pending_node
-                    del StaticDepGraph.pending_nodes[pending_node.insn]
+                    del StaticDepGraph.pending_nodes[func][pending_node.insn]
                     #FIXME: remoe after?
 
             target_bbs.add(graph.cfg.ordered_bbs[0])
