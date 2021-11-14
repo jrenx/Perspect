bb6d068f9ec824f4b3173170a1b3f965f9d998db
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-11-14 18:04:04 -0500

    [mongodb-57221] Making fast repro event faster...
    Former-commit-id: 47c0f93536e275fbb6fcfc52bea33035b1db8c34

--- repro/setup_fast.js
+++ repro/setup_fast.js
@@ -3,11 +3,11 @@
     db.dropDatabase();
 
     const key = 'value';
-    const DOCS = 10 * 500;
-    for (let i = 0; i < DOCS / 500; i++) {
+    const DOCS = 10 * 100;
+    for (let i = 0; i < DOCS / 100; i++) {
         let bulk = db.coll.initializeUnorderedBulkOp();
-        for (let j = 0; j < 500; j++) {
-            let val = i * 500 + j;
+        for (let j = 0; j < 100; j++) {
+            let val = i * 100 + j;
                 bulk.insert({a: key, b: val, c: val})
             }
         bulk.execute();
