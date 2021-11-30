242c957ec6e2e61b9cbb102f1f584edfb02a35b9
Sitao Wang <sitaowang1998@outlook.com>
2021-11-29 20:30:18 -0500

    Added gdb script to cound missing cursor
    Former-commit-id: 6fc31b5ce2e97efd098dd99b8e1bea6fa1846681

---
+++ cursor_miss.py
@@ -0,0 +1,43 @@
+import json
+
+import gdb
+
+gdb.execute("br bt_cursor.c:677")
+gdb.execute("br bt_cursor.c:693")
+
+valid = 0
+invalid = 0
+
+def br_handler(event):
+    stack = gdb.execute("i stack")
+    if 'doTTLForIndex' not in stack:
+        return
+    
+    br_num = event.breakpoints[-1]
+    if br_num == 1:
+        global valid
+        valid += 1
+    elif br_num ==2:
+        global invalid
+        invalid += 1
+
+gdb.event.stop.connect(br_handler)
+
+not_exit = True
+
+def exit_handler(event):
+    global not_exit
+    not_exit = False
+    
+gdb.events.exited.connect(exit_handler)
+
+while not_exit:
+    try:
+        gdb.execute('c')
+    except Exception:
+        break
+
+with open('cursur_miss.txt', 'w') as f:
+    f.write(json.dumps({'valid': valid, 'invalid': invalid}, f))
+
+gdb.execute('quit', False, True)
