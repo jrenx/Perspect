2adc5cff492e5637e32701058326cca99ea41ae5
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-11-30 23:26:22 -0500

    [mongodb_56724] Updating repro script to avoid assertion error as per Sitaos suggestion.
    Former-commit-id: f2052bdd80f4b9046e38a16175ff685cb59590f8

--- repro/repro_slow.js
+++ repro/repro_slow.js
@@ -3,7 +3,8 @@
     db.coll.createIndex({t: -1}, {expireAfterSeconds: 0});
     db.adminCommand({setParameter: 1, ttlMonitorEnabled: true});
     const ttlPasses = db.serverStatus().metrics.ttl.passes;
-    assert.soon(() => {
-        return db.serverStatus().metrics.ttl.passes >= ttlPasses + 2;
-    })
+    while (db.serverStatus().metrics.ttl.passes < ttlPasses + 2) {
+        sleep(1000);
+    }
+    db.getSiblingDB("admin").shutdownServer();
 }) ();
