6b4d0ec073c4020cae4822229240b03d9caf986d
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2021-12-02 14:58:14 -0500

    [mongodb_56724] Updating repros to add busy waiting loop at the end per Sitaos instruction.
    Former-commit-id: 94e848e648d9c71dd39ac92e4062dcd969552972

--- repro/repro_fast.js
+++ repro/repro_fast.js
@@ -3,7 +3,10 @@
     db.coll.createIndex({t: 1}, {expireAfterSeconds: 0});
     db.adminCommand({setParameter: 1, ttlMonitorEnabled: true});
     const ttlPasses = db.serverStatus().metrics.ttl.passes;
-    assert.soon(() => {
-        return db.serverStatus().metrics.ttl.passes >= ttlPasses + 2;
-    })
+    var t = new Date();
+    while (db.serverStatus().metrics.ttl.passes < ttlPasses + 2) {
+        sleep(1000);
+    }
+    print(new Date() - t);
+    db.getSiblingDB("admin").shutdownServer();
 }) ();
