980f7fda66e7fd698f0daa6718dadd42fa1d35b9
renxian2 <renxian2@baker21.dsrg.utoronto.ca>
2021-12-01 19:49:52 -0500

    [rr] logging change.
    Former-commit-id: a0b89ec4b15317f4c4c29cd050df7d6ecd8eb4f4

--- rr/get_breakpoints.py
+++ rr/get_breakpoints.py
@@ -78,7 +78,7 @@ def run_breakpoint(breakpoints, reg_points, regs, off_regs, offsets, shifts, src
               'target': target}
     with open(os.path.join(rr_dir, 'config.json'), 'w') as f:
         json.dump(config, f)
-    print("Timeout is: " + str(timeout), flush=True)
+    print("[rr][" + pid + "]Timeout is: " + str(timeout), flush=True)
     success = True
     a = datetime.datetime.now()
     rr_process = subprocess.Popen('rr replay --cpu-unbound', stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, shell=True)
