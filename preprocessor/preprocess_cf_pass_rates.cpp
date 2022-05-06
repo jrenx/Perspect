692512a5f9103f747010e885d08f4ae58f0ff398
renxian2 <renxian2@baker17.dsrg.utoronto.ca>
2022-05-06 19:21:39 -0400

    [compare_relation/pass_rates] Forgot to add file: preprocess_cf_pass_rates.cpp
    Former-commit-id: f1d7643846b27fb48b2729b71b6a2f7545a2dcec

---
+++ preprocessor/preprocess_cf_pass_rates.cpp
@@ -0,0 +1,38 @@
+#include <iostream>     // std::cout
+#include <fstream>      // std::ifstream
+#include <cstring>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include "cJSON.h"
+#include <boost/unordered_set.hpp>
+#include <boost/unordered_map.hpp>
+#include <chrono>
+#include <bitset>
+#define CF_PASS_RATE
+#include "parser.cpp"
+using namespace std;
+using namespace boost;
+//TODO: fix all the weird casings in this file.
+
+int main(int argc, char *argv[]) {
+
+  int pa_id = -1;
+  //if (argc >= 2) {
+  //  char *pa_str = (char *) argv[1];
+  //  pa_id = atoi(pa_str);
+  //}
+  cout << pa_id << endl;
+
+  Parser p = Parser();
+  p.initData(pa_id);
+
+  unsigned long length;
+  std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+  char *buffer = Parser::readFile(p.traceFile, length);
+  cout << "Read " << length << " characters... " << endl;
+  std::chrono::steady_clock::time_point t3 = std::chrono::steady_clock::now();
+  std::cout << "Reading file took = " << std::chrono::duration_cast<std::chrono::seconds>(t3 - t2).count() << "[s]" << std::endl;
+
+  p.parse(pa_id, length, buffer);
+}
