20b956fe5da147f9abb75aeddd4b53c064b28120
renxian2 <renxian2@baker19.dsrg.utoronto.ca>
2022-12-04 22:18:27 -0500

    [dynamic_dep_graph] Just put a warning about usage of generating dynamic callsites for now.
    Former-commit-id: a8042acd91a9efdc7d057fe69f97f7618766b30d

--- dynamic_dep_graph.py
+++ dynamic_dep_graph.py
@@ -555,6 +555,7 @@ class DynamicDependence:
         print("[dyn_dep] Total number of instructions watched: " + str(len(instructions)))
         print(instructions)
         trace.run_function_trace(instructions)
+        print("NEED TO RUN THE ABOVE INSTRUCTION MANUALLY and replace instruction_reg_log.so with instruction_reg_deref_log.so")
         return
 
     def build_static_dependencies(self, starting_events, prog, sa_steps=10000):
