d4eef9a34c983af03dea7581190723d7ddb504c5
renxian2 <renxian2@baker21.dsrg.utoronto.ca>
2022-12-09 11:20:20 -0500

    [mongodb_44991_slow] Updating arguments to generate_indices.sh
    Former-commit-id: b6b2b6d9dc4fa13d8dcaf4bf8f9f3b0c34c22639

--- generate_indices.sh
+++ generate_indices.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3.7 static_dep_graph.py -in -s_ours /home/sitao/mongoDB/source/4.2.1/
+python3.7 static_dep_graph.py -in -s_ours /home/sitao/mongoDB/source/4.2.1/ -b_ours binary_4.2.1
