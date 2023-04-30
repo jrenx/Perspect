a6cc176e5bd148557253d294ca8993c078dc2064
renxian2 <renxian2@baker7.dsrg.utoronto.ca>
2023-04-29 23:00:07 -0400

    [go_8832] Use go's calling convention.

--- binary_analysis/util.cpp
+++ binary_analysis/util.cpp
@@ -41,7 +41,7 @@ bool DEBUG_SLICE = false;
 bool DEBUG_BIT = false;
 bool DEBUG_STACK = false;
 bool CRASH_ON_ERROR = false;
-bool USE_X86_CALLING_CONVENTION = true;
+bool USE_X86_CALLING_CONVENTION = false;
 //#define IGNORE_GO_FUNCTION_ENTRY
 
 boost::unordered_map<std::string, std::string> regMap =
